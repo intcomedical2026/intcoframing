8066e9ccf3ef5499cb97f7c20a4b4da9076f9f6d
孔俊豪 <kongjunhao@intco.com.cn>
2026-06-26 13:06:21 +0800

    Add Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export const viewport: Viewport = {
 };
 
 const supportedHtmlLangs = new Set(["en", "es", "pt", "fr", "de", "ja"]);
+const CLARITY_PROJECT_ID = "xcusp9mr3y";
 
 export default async function RootLayout({
   children,
@@ -63,6 +64,9 @@ export default async function RootLayout({
         {children}
         <Script id="intco-gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NFFXV4DP');`}
+        </Script>
+        <Script id="intco-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","${CLARITY_PROJECT_ID}");`}
         </Script>
         <Script id="hs-script-loader" src={`https://js-${HUBSPOT_REGION}.hs-scripts.com/${HUBSPOT_PORTAL_ID}.js`} strategy="afterInteractive" />
       </body>
